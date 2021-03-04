79fff5ddeeb765b2a5698b73cd00546bf0d1e685
Jeongmin <tdcian71@gmail.com>
2021-03-05 07:15:58 +0900

    Initial Commit

---
+++ FastAndSafeMemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FastAndSafeMemo
+//
+//  Created by APPLE on 2021/03/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
