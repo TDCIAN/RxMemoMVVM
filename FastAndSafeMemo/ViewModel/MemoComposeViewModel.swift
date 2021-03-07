d9a4db285ccb052c91bb8d8ece4ab8c4d20295dd
Jeongmin <tdcian71@gmail.com>
2021-03-07 21:06:18 +0900

    implement MemoComposeViewModel

--- FastAndSafeMemo/ViewModel/MemoComposeViewModel.swift
+++ FastAndSafeMemo/ViewModel/MemoComposeViewModel.swift
@@ -6,8 +6,39 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+import Action
 
-// 메모를 작성할 때 사용될 뷰모델
-class MemoComposeViewModel {
+// 이 모델은 컴포즈신에서 사용한다 컴포즈신은 새로운 메모를 추가할 때, 메모를 편집할 때 공통적으로 사용한다
+class MemoComposeViewModel: CommonViewModel {
+    private let content: String? // 새로운 메모일 때는 닐, 기존 메모일 때는 기존 메모
     
+    var initialText: Driver<String?> {
+        return Observable.just(content).asDriver(onErrorJustReturn: nil)
+    }
+    
+    let saveAction: Action<String, Void>
+    let cancelAction: CocoaAction
+    
+    init(title: String, content: String? = nil, sceneCoordinator: SceneCoordinatorType, storage: MemoStorageType, saveAction: Action<String, Void>? = nil, cancelAction: CocoaAction? = nil) {
+        self.content = content
+        
+        self.saveAction = Action<String, Void> { input in
+            if let action = saveAction {
+                action.execute(input)
+            }
+            
+            return sceneCoordinator.close(animated: true).asObservable().map { _ in }
+            
+        }
+        
+        self.cancelAction = CocoaAction {
+            if let action = cancelAction {
+                action.execute(())
+            }
+            return sceneCoordinator.close(animated: true).asObservable().map { _ in }
+        }
+        super.init(title: title, sceneCoordinator: sceneCoordinator, storage: storage)
+    }
 }
