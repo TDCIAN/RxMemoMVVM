cc389d3398a2154b60b5007b0f54bc327228a8ec
Jeongmin <tdcian71@gmail.com>
2021-03-07 21:29:43 +0900

    Compose scene - write mode

--- FastAndSafeMemo/ViewController/MemoComposeViewController.swift
+++ FastAndSafeMemo/ViewController/MemoComposeViewController.swift
@@ -6,10 +6,20 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import Action
+import NSObject_Rx
 
 class MemoComposeViewController: UIViewController, ViewModelBindableType {
 
     var viewModel: MemoComposeViewModel!
+    
+    @IBOutlet weak var cancelButton: UIBarButtonItem!
+    
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+    
+    @IBOutlet weak var contentTextView: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,7 +28,35 @@ class MemoComposeViewController: UIViewController, ViewModelBindableType {
     }
     
     func bindViewModel() {
+        viewModel.title
+            .drive(navigationItem.rx.title)
+            .disposed(by: rx.disposeBag)
         
+        // 메모 쓰기 모드에서는 빈 문자열이, 메모 편집 모드에서는 편집할 메모가 나온다
+        viewModel.initialText
+            .drive(contentTextView.rx.text)
+            .disposed(by: rx.disposeBag)
+        
+        cancelButton.rx.action = viewModel.cancelAction
+        
+        // 반복 탭을 막기 위해 쓰로틀 사용한다 (0.5초)
+        saveButton.rx.tap.throttle(.milliseconds(500), scheduler: MainScheduler.instance)
+            .withLatestFrom(contentTextView.rx.text.orEmpty)
+            .bind(to: viewModel.saveAction.inputs)
+            .disposed(by: rx.disposeBag)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        contentTextView.becomeFirstResponder()
     }
 
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        if contentTextView.isFirstResponder {
+            contentTextView.resignFirstResponder()
+        }
+    }
 }
